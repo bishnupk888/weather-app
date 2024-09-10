ced6d7c74d0032581897444d12d2595574338b35
bishnupk888 <bishnupk888@gmail.com>
2024-09-10 21:19:10 +0530

    updates made in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ function App() {
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
+        
         const forecastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
