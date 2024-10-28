db543e8571b594defd78863d7f5fbc248c552a0f
Bishnu PK <130635787+bishnupk888@users.noreply.github.com>
2024-10-28 11:02:03 +0530

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,15 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'bfdb35ddd8mshd618f60a4ec1789p1355bejsn91069a5f7b90',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/'
+
+export const WEATHER_API_KEY = '896b6b743419cc9e26cff0f08a5a6224'  
+
+export const PIXABAY_API_KEY ='45856738-32fb5b1af6f314069408de067'  
